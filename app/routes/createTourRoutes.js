219902084146aca23ad1da9b6491ffe2d80dddd3
Rashmil Panchani <32737711+Rashmil-1999@users.noreply.github.com>
2019-09-30 19:40:32 +0530

    path changed

--- app/routes/createTourRoutes.js
+++ app/routes/createTourRoutes.js
@@ -67,10 +67,10 @@ module.exports = function(app) {
         }
       }
       if (i === allPlaces.length) {
-        console.log(`C:/programming/WD/public/IMAGEUPLOADS/${image.name}`);
-        image.mv(
+        console.log(
           `C:/Users/priyal/Desktop/wd_project/public/IMAGEUPLOADS/${image.name}`
         );
+        image.mv(`C:/programming/WD/public/IMAGEUPLOADS/${image.name}`);
         await tourModule.createPlace(place);
       } else {
         let tour_types = await tourModule.getAllTourTypes();
@@ -110,7 +110,7 @@ module.exports = function(app) {
       data.t_id = parseInt(tourid, 10);
       console.log(tourid, data.t_id);
       itineraryFile.mv(
-        `C:/Users/priyal/Desktop/wd_project/public/IMAGEUPLOADS/${itineraryFile.name}`
+        `C:/programming/WD/public/IMAGEUPLOADS/${itineraryFile.name}`
       );
       await tourModule.insertTourItinerary(data);
       res.redirect("/admin/create-tour");
