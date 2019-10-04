0f2f54487583b7ea83f248062633585024057b3d
Rashmil Panchani <32737711+Rashmil-1999@users.noreply.github.com>
2019-10-04 20:15:50 +0530

    bug fix

--- app/routes/tourRoutes.js
+++ app/routes/tourRoutes.js
@@ -28,7 +28,7 @@ module.exports = function(app) {
     let description = utils.convertToArray(tour[0].description);
     console.log(tour[0].t_id);
     let file_path = await tourModule.getFilePath(tour[0].t_id);
-    console.log(file_path.file_path);
+    // console.log(file_path.file_path);
     res.render("itinerary", {
       tour: tour[0],
       itinerary: itinerary,
@@ -37,7 +37,7 @@ module.exports = function(app) {
       dates: dates,
       items: items,
       costs: costs,
-      file_path: file_path.file_path,
+      file_path: file_path,
       user: req.user
     });
   });
